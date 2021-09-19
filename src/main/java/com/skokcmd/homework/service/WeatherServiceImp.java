27a8b33437a560f1418c3ce6611ffa450f1b21a1
Boris Skok <skokcmd@gmail.com>
2021-09-19 12:30:32 +0200

    comment

--- src/main/java/com/skokcmd/homework/service/WeatherServiceImp.java
+++ src/main/java/com/skokcmd/homework/service/WeatherServiceImp.java
@@ -6,29 +6,28 @@ import com.github.prominence.openweathermap.api.enums.UnitSystem;
 import org.springframework.stereotype.Service;
 
 @Service
-public class WeatherServiceImp implements  WeatherService {
+public class WeatherServiceImp implements WeatherService {
 
-  private final OpenWeatherMapClient openWeatherClient =
-      new OpenWeatherMapClient("9f22fb91f6b670fd083a7e74ea8922aa");
+    private final OpenWeatherMapClient openWeatherClient =
+            new OpenWeatherMapClient("9f22fb91f6b670fd083a7e74ea8922aa"); // asi by bylo lepsi ulozit do secret souboru
 
-  /**
-   * Vrati teplotu podle mesta
-   *
-   * @param city mesto
-   * @return teplota
-   *
-   * */
-  @Override
-  public double getTemperatureByCity(String city) {
-    return openWeatherClient
-        .currentWeather()
-        .single()
-        .byCityName(city)
-        .language(Language.CZECH)
-        .unitSystem(UnitSystem.METRIC)
-        .retrieve()
-        .asJava()
-        .getTemperature()
-        .getValue();
-  }
+    /**
+     * Vrati teplotu podle mesta
+     *
+     * @param city mesto
+     * @return teplota
+     */
+    @Override
+    public double getTemperatureByCity(String city) {
+        return openWeatherClient
+                .currentWeather()
+                .single()
+                .byCityName(city)
+                .language(Language.CZECH)
+                .unitSystem(UnitSystem.METRIC)
+                .retrieve()
+                .asJava()
+                .getTemperature()
+                .getValue();
+    }
 }
